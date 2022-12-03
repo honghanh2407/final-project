01c7014dc1618563d4f073b58742a1ac8495c400
honghanh2407 <honghanh24072005sutu@gmail.com>
2022-12-03 15:18:16 +0700

    final project

---
+++ WEB BASIC_FINAL PROJECT/index.js
@@ -0,0 +1,21 @@
+function turnBanner_1 () {
+    document.getElementById ("pic").src = "./banner/banner_1.jpg"
+}
+function turnBanner_2 () {
+    document.getElementById ("pic").src = "./banner/banner_2.jpg"
+}
+function turnBanner_3 () {
+    document.getElementById ("pic").src = "./banner/banner_3.jpg"
+}
+function turnBanner_4 () {
+    document.getElementById ("pic").src = "./banner/banner_4.jpg"
+}
+function turnBanner_5 () {
+    document.getElementById ("pic").src = "./banner/banner_5.jpg"
+}
+function turnBanner_6 () {
+    document.getElementById ("pic").src = "./banner/banner_6.jpg"
+}
+function turnBanner_7 () {
+    document.getElementById ("pic").src = "./banner/banner_7.jpg"
+}
